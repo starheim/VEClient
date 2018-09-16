7141a3f09af7bf8a35c46281c4bd3cea281b586d
starheim <wstarvall@gmail.com>
2018-09-16 23:17:19 +0200

    Add files via upload

---
+++ VEChat/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetMQ.Sockets;
+using NetMQ;
+
+
+namespace VEChat
+{
+    class Program
+    {
+        public static void ChatReceiveThread()
+        {
+            String incomingMessage;
+
+            using (var VEReceiveSocket = new RequestSocket(">tcp://206.189.9.238:5563"))
+            {
+                VEReceiveSocket.SendFrame("");
+
+                while (true)
+                {
+                    incomingMessage = VEReceiveSocket.ReceiveFrameString();
+                    Console.WriteLine(incomingMessage);
+                    VEReceiveSocket.SendFrame("");
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            String outgoingMessage;
+
+            using (var VESendSocket = new RequestSocket(">tcp://206.189.9.238:5560"))
+            {
+                ThreadStart threadRef = new ThreadStart(ChatReceiveThread);
+
+                Thread thread = new Thread(threadRef);
+
+                thread.Start();
+
+                Console.WriteLine("Connecting to chat");
+                VESendSocket.SendFrame("");
+                String answer = VESendSocket.ReceiveFrameString();
+                Console.WriteLine(answer);
+
+                while (true)
+                {
+                    outgoingMessage = Console.ReadLine();
+
+                    VESendSocket.SendFrame(outgoingMessage);
+
+                    answer = VESendSocket.ReceiveFrameString();
+                }
+            }
+        }
+    }
+}
---
+++ VEClient/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetMQ.Sockets;
+using NetMQ;
+
+
+namespace VEClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var server = new RequestSocket(">tcp://206.189.9.238:5555"))
+            {
+                String serverMessage;
+
+                using (var process1 = new Process())
+                {
+                    process1.StartInfo.FileName = @"..\..\..\VEChat\bin\Debug\VEChat.exe";
+                    process1.Start();
+                }
+
+                Console.WriteLine("Visual educator client.");
+                //Connecting to server
+                server.SendFrame("");
+                //Receiving connection confirmation
+                serverMessage = server.ReceiveFrameString();
+
+                Console.WriteLine(serverMessage);
+
+                server.SendFrame("");
+                //Confirmation both parties are connected
+                serverMessage = server.ReceiveFrameString();
+
+                Console.WriteLine(serverMessage);
+
+                Console.WriteLine("Press enter to initiate the examination");
+
+                Console.ReadLine();
+
+                server.SendFrame("");
+
+                serverMessage = server.ReceiveFrameString();
+
+                Console.WriteLine(serverMessage);
+
+                server.SendFrame("");
+
+                serverMessage = server.ReceiveFrameString();
+
+                Console.WriteLine(serverMessage);
+
+                Console.ReadLine();
+            }
+            
+
+            
+
+
+        }
+    }
+}
